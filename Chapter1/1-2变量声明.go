90283a1179f9e40a0313507698c79dbf7b749b4f
张从龙 <zhangconglong@163.com>
2022-07-17 18:39:46 +0800

    <build>: 调整项目结构

---
+++ Chapter1/1-2变量声明.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age = 1
+	name := "张从龙"
+
+	fmt.Printf(string(age))
+	fmt.Printf(name)
+}
--- Chapter1/1-3局部变量与全局变量.go
+++ Chapter1/1-3局部变量与全局变量.go
@@ -2,13 +2,16 @@ package main
 
 import "fmt"
 
+//全局变量
 var msg = "去外面吃点好的。"
 
 func main() {
+	//局部变量
 	msg := "main函数内"
 	{
 		msg := "在家吃点好的"
 		fmt.Println(msg)
 	}
+
 	fmt.Println(msg)
 }
