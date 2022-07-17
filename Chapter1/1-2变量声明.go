66c517a35b6c4fd5491a3f56669fd2d5295dd44f
张从龙 <zhangconglong123@163.com>
2022-07-17 18:46:37 +0800

    <feat>: int转字符串

--- Chapter1/1-2变量声明.go
+++ Chapter1/1-2变量声明.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var age = 1
 	name := "张从龙"
 
-	fmt.Printf(string(age))
+	fmt.Printf(strconv.Itoa(age))
 	fmt.Printf(name)
 }
